c12cf99b23506e2137bd85e7942d3622b164127c
Erik Bryn <erik.bryn@gmail.com>
2016-07-04 08:14:27 -0700

    Working inside of apps

--- index.js
+++ index.js
@@ -6,23 +6,5 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = UITheme.extend({
-  name: 'ui-base-theme',
-
-  included: function(app, parentAddon) {
-    this._super.included(app);
-
-    var target = (parentAddon || app);
-    var fontsPath = ('vendor/font-awesome/fonts');
-
-    app.import('vendor/normalize.css');
-
-    fs.readdirSync(fontsPath).forEach(function(fontFilename){
-      target.import(
-        path.join(fontsPath, fontFilename),
-        { destDir:'/fonts' }
-      );
-    });
-
-    app.import('vendor/font-awesome/css/font-awesome.css');
-  }
+  name: 'ui-base-theme'
 });
