3dce773454bda0b799e038530af5555b9d0a9325
Eric Kelly <heroiceric@gmail.com>
2016-07-06 15:56:05 -0400

    Basic generated styleguide from tempaltes

---
+++ tests/dummy/app/controllers/styleguide.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  allComponents: [
+    'ui-button-group',
+    'ui-button',
+    'ui-dropbutton',
+    'ui-checkbox',
+    'ui-tabs',
+    'ui-pagination'
+  ]
+});
