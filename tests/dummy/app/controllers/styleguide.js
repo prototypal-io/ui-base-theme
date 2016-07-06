110bbfda6ea66a4b763582478643d2a76bcaff18
Eric Kelly <heroiceric@gmail.com>
2016-07-06 17:35:04 -0400

    WIP - Generated styleguide with selectable state and kind

--- tests/dummy/app/controllers/styleguide.js
+++ tests/dummy/app/controllers/styleguide.js
@@ -2,11 +2,45 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   allComponents: [
-    'ui-button-group',
-    'ui-button',
-    'ui-dropbutton',
-    'ui-checkbox',
-    'ui-tabs',
-    'ui-pagination'
+    {
+      name: 'ui-button',
+      kinds: [
+        'default',
+        'material',
+        'primary',
+        'simple'
+      ],
+      states: [
+        'active',
+        'focus',
+        'disabled',
+        'loading'
+      ]
+    },
+    {
+      name: 'ui-button-group',
+      kinds: ['default'],
+      states: []
+    },
+    {
+      name: 'ui-dropbutton',
+      kinds: ['default'],
+      states: []
+    },
+    {
+      name: 'ui-checkbox',
+      kinds: ['default'],
+      states: []
+    },
+    {
+      name: 'ui-tabs',
+      kinds: ['default'],
+      states: []
+    },
+    {
+      name: 'ui-pagination',
+      kinds: ['default'],
+      states: []
+    }
   ]
 });
