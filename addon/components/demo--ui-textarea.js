53455b195b4ab14f500fce554abeaaf3314f711b
Eric Kelly <heroiceric@gmail.com>
2016-07-06 20:55:25 -0400

    Improve ui-textarea demo

---
+++ addon/components/demo--ui-textarea.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from 'ui-base-theme/templates/components/demo--ui-textarea';
+
+export default Ember.Component.extend({
+  tagName: '',
+  layout
+});
--- tests/dummy/app/controllers/styleguide.js
+++ tests/dummy/app/controllers/styleguide.js
@@ -6,7 +6,7 @@ let uiComponentNames = uiComponentModules.map(m => m.replace(/^ui-base-theme\/co
 Ember.A(uiComponentNames).removeObjects(['ui-component', 'ui-demo', 'ui-icon', 'ui-kind', 'ui-modal-backdrop', 'ui-modal', 'ui-panel-content', 'ui-panel-titlebar', 'ui-popup', 'ui-table-cell', 'ui-table-row', 'ui-table-layout', 'ui-table', 'ui-tooltip', 'ui-ripple', 'ui-ripple-animation', 'ui-prevent-scroll-outside']);
 
 let kinds = ['default'];
-let states = ['active', 'focus', 'disabled', 'loading'];
+let states = ['active', 'focus', 'disabled', 'loading', 'error'];
 let allComponents = uiComponentNames.map(cn => {
   return {
     name: cn,
