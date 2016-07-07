bf9223dc5bd6343516a29043c466ce898f1d6276
Eric Kelly <heroiceric@gmail.com>
2016-07-07 12:26:12 -0400

    Show code snippets in styleguide

--- tests/dummy/app/controllers/styleguide.js
+++ tests/dummy/app/controllers/styleguide.js
@@ -1,7 +1,7 @@
 /* globals require */
 import Ember from 'ember';
 
-let uiComponentModules = Object.keys(require.entries).filter((module) => /^ui-base-theme\/components\/ui-/.test(module));
+let uiComponentModules = Object.keys(requirejs.entries).filter((module) => /^ui-base-theme\/components\/ui-/.test(module));
 let uiComponentNames = uiComponentModules.map(m => m.replace(/^ui-base-theme\/components\//, '')).filter(m => !/--/.test(m));
 Ember.A(uiComponentNames).removeObjects(['ui-component', 'ui-demo', 'ui-icon', 'ui-kind', 'ui-modal-backdrop', 'ui-modal', 'ui-panel-content', 'ui-panel-titlebar', 'ui-popup', 'ui-table-cell', 'ui-table-row', 'ui-table-layout', 'ui-table', 'ui-tooltip', 'ui-ripple', 'ui-ripple-animation', 'ui-prevent-scroll-outside']);
 
