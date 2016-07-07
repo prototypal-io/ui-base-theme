ea8342d4f88063ee7aaf3e855a71d68cc656f282
Eric Kelly <heroiceric@gmail.com>
2016-07-07 13:29:55 -0400

    Don't show ui-dropbutton-trigger in styleguide

--- tests/dummy/app/controllers/styleguide.js
+++ tests/dummy/app/controllers/styleguide.js
@@ -1,9 +1,33 @@
 /* globals require */
 import Ember from 'ember';
 
-let uiComponentModules = Object.keys(requirejs.entries).filter((module) => /^ui-base-theme\/components\/ui-/.test(module));
-let uiComponentNames = uiComponentModules.map(m => m.replace(/^ui-base-theme\/components\//, '')).filter(m => !/--/.test(m));
-Ember.A(uiComponentNames).removeObjects(['ui-component', 'ui-demo', 'ui-icon', 'ui-kind', 'ui-modal-backdrop', 'ui-modal', 'ui-panel-content', 'ui-panel-titlebar', 'ui-popup', 'ui-table-cell', 'ui-table-row', 'ui-table-layout', 'ui-table', 'ui-tooltip', 'ui-ripple', 'ui-ripple-animation', 'ui-prevent-scroll-outside']);
+let uiComponentModules = Object.keys(requirejs.entries)
+  .filter((module) => /^ui-base-theme\/components\/ui-/.test(module));
+
+let uiComponentNames = uiComponentModules
+  .map(m => m.replace(/^ui-base-theme\/components\//, ''))
+  .filter(m => !/--/.test(m));
+
+Ember.A(uiComponentNames).removeObjects([
+  'ui-component',
+  'ui-demo',
+  'ui-dropbutton-trigger',
+  'ui-icon',
+  'ui-kind',
+  'ui-modal',
+  'ui-modal-backdrop',
+  'ui-panel-content',
+  'ui-panel-titlebar',
+  'ui-popup',
+  'ui-prevent-scroll-outside',
+  'ui-ripple',
+  'ui-ripple-animation',
+  'ui-table',
+  'ui-table-cell',
+  'ui-table-layout',
+  'ui-table-row',
+  'ui-tooltip'
+]);
 
 let kinds = ['default'];
 let states = ['active', 'focus', 'disabled', 'loading', 'error'];
