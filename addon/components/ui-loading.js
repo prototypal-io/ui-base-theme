f2186cf956eb85fd84723d9e5fa284133da86bf6
Jaco Joubert <joubert.jaco@gmail.com>
2016-07-08 11:37:57 -0400

    add ui-loading component #77

---
+++ addon/components/ui-loading.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import UIComponent from './ui-component';
+import layout from 'ui-base-theme/templates/components/ui-loading';
+
+export default UIComponent.extend({
+  layout
+});
