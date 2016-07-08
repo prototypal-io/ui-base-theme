b628cc82ee6fea2f29113004d347009bf81a8b5f
Jaco Joubert <joubert.jaco@gmail.com>
2016-07-08 10:32:24 -0400

    kill baseURL in dev

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,6 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -29,7 +28,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
