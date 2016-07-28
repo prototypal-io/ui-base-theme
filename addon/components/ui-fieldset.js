781822f84f29927af9c3f3ccd7cfe0a9cee77440
Jaco Joubert <joubert.jaco@gmail.com>
2016-07-28 13:06:00 -0400

    add fieldset component

---
+++ addon/components/ui-fieldset.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import UIComponent from './ui-component';
+import layout from '../templates/components/ui-fieldset';
+
+export default UIComponent.extend({
+  layout
+});
