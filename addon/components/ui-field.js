6c60d9322a73b0ecdee8983333ef4fc6fec9bb38
Jaco Joubert <joubert.jaco@gmail.com>
2016-07-28 14:03:38 -0400

    add active state to ui-field

--- addon/components/ui-field.js
+++ addon/components/ui-field.js
@@ -6,6 +6,7 @@ export default UIComponent.extend({
   layout,
 
   type: 'input',
+  active: false,
 
   generatedInputId: Ember.computed(function() {
     return `${Ember.guidFor(this)}--input`;
@@ -19,9 +20,20 @@ export default UIComponent.extend({
     return `ui-${this.get('type')}`;
   }),
 
-  states: Ember.computed('disabled', function() {
+  states: Ember.computed('disabled', 'active', function() {
     return {
-      disabled: this.get('disabled')
+      disabled: this.get('disabled'),
+      active: this.get('active')
     };
-  })
+  }),
+
+  actions: {
+    focusIn() {
+      this.set('active', true);
+    },
+
+    focusOut() {
+      this.set('active', false);
+    }
+  }
 });
