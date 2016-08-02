8ba790a7083edc7934b5d9e2037e5cb2fb230974
Eric Kelly <heroiceric@gmail.com>
2016-08-02 14:03:04 -0700

    Update naming conventions for ui-dropbutton

--- addon/components/ui-dropbutton---trigger.js
+++ addon/components/ui-dropbutton---trigger.js
@@ -1,5 +1,5 @@
 import UIComponent from './ui-component';
-import layout from 'ui-base-theme/templates/components/ui-dropbutton-trigger';
+import layout from 'ui-base-theme/templates/components/ui-dropbutton---trigger';
 
 export default UIComponent.extend({
   layout
