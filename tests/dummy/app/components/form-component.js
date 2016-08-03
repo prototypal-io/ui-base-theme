bf0f0cad0fdc87609511be8e13da880b33ccf787
Jaco Joubert <joubert.jaco@gmail.com>
2016-08-03 12:11:14 -0400

    refine interactions for form components - show tooltip on right side - only switch out of edit mode for multi-input components

--- tests/dummy/app/components/form-component.js
+++ tests/dummy/app/components/form-component.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  wasFocused: false,
+
   actions: {
     focusIn() {
       this.$().find('input:first').focus();
+      this.set('wasFocused', true);
     }
   }
 });
