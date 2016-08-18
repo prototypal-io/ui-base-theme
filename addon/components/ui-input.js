31ad93f8fdae8d9e3f0aed6beccb9670ec6fe649
Jaco Joubert <joubert.jaco@gmail.com>
2016-08-18 15:43:24 -0400

    add error handling to ui-input

--- addon/components/ui-input.js
+++ addon/components/ui-input.js
@@ -7,11 +7,50 @@ export default UIComponent.extend({
 
   disabled: false,
   error: false,
+  forceErrorDisplay: false,
 
-  states: Ember.computed('disabled', 'error', function() {
+  _wasFocused: false,
+  _previousErrorState: null,
+
+  states: Ember.computed('disabled', '_errorState', function() {
     return {
       disabled: this.get('disabled'),
-      error: this.get('error')
+      error: this.get('_errorState')
     };
-  })
+  }),
+
+  _errorState: Ember.computed('error', '_wasFocused', 'forceErrorDisplay', 'value', function() {
+    const wasFocused = this.get('_wasFocused');
+    const error = this.get('error');
+    const forceErrorDisplay = this.get('forceErrorDisplay');
+    const previousErrorState = this.get('_previousErrorState');
+    let errorState = false;
+
+    if (error && (wasFocused || previousErrorState || forceErrorDisplay)) {
+      errorState = {
+        hasError: true,
+        message: typeof error === 'string' ? error : null
+      };
+    }
+
+    return this.set('_previousErrorState', errorState);
+  }),
+
+  actions: {
+    onFocusIn() {
+      this.set('_wasFocused', false);
+
+      if (this.attrs.onFocusIn) {
+        this.attrs.onFocusIn();
+      }
+    },
+
+    onFocusOut() {
+      this.set('_wasFocused', true);
+
+      if (this.attrs.onFocusOut) {
+        this.attrs.onFocusOut();
+      }
+    }
+  }
 });
