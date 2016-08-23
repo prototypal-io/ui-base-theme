647e430349e0fa1773034251c4af2e3a9f0e428b
Eric Kelly <heroiceric@gmail.com>
2016-08-23 11:17:32 -0700

    Update ui-checkbox to work with theme system

--- addon/components/ui-checkbox.js
+++ addon/components/ui-checkbox.js
@@ -1,18 +1,56 @@
 import Ember from 'ember';
 import UIValidated from './ui-validated';
 import layout from '../templates/components/ui-checkbox';
+import state from '../state';
 
 export default UIValidated.extend({
   layout,
 
-  disabled: false,
+  disabled: state({ defaultValue: false }),
+  focused: state({ defaultValue: false }),
+
   value: false,
 
-  states: Ember.computed('disabled', 'errorState', 'value', function() {
+  states: Ember.computed(
+    'disabled',
+    'errorState',
+    'focused',
+    'hover',
+    'value',
+  function() {
     return {
+      checked: this.get('value'),
       disabled: this.get('disabled'),
+      enabled: !this.get('disabled'),
       error: this.get('errorState'),
-      checked: this.get('value')
+      focused: this.get('focused'),
+      hover: this.get('hover')
     };
-  })
+  }),
+
+  actions: {
+    onFocusIn() {
+      this._super(...arguments);
+
+      this.set('focused', true);
+    },
+
+    onFocusOut() {
+      this._super(...arguments);
+
+      this.set('focused', false);
+    },
+
+    onMouseEnter() {
+      this._super(...arguments);
+
+      this.set('hover', true);
+    },
+
+    onMouseLeave() {
+      this._super(...arguments);
+
+      this.set('hover', false);
+    }
+  }
 });
