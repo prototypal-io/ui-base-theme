fda1e8110fc96d88b0983bae20122b9585a3c16d
Eric Kelly <heroiceric@gmail.com>
2016-08-26 09:10:51 -0700

    Add theme prefix to ui-list & friends

--- addon/components/ui-list---item.js
+++ addon/components/ui-list---item.js
@@ -2,5 +2,15 @@ import UIComponent from './ui-component';
 import layout from 'ui-base-theme/templates/components/ui-list---item';
 
 export default UIComponent.extend({
-  layout
+  layout,
+
+  theme: 'base',
+
+  actions: {
+    onclick() {
+      if (this.attrs.onclick) {
+        this.attrs.onclick(...arguments);
+      }
+    }
+  }
 });
