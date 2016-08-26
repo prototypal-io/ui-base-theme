c7f9b6af1d0ae847b790ab5b5ffa1bc6ae77c2ea
Eric Kelly <heroiceric@gmail.com>
2016-08-26 10:22:02 -0700

    Add theme prefix to ui-tabs

--- addon/components/ui-tabs.js
+++ addon/components/ui-tabs.js
@@ -5,6 +5,8 @@ import layout from '../templates/components/ui-tabs';
 export default UIComponent.extend({
   layout,
 
+  theme: 'base',
+
   classes: Ember.computed('class', 'size', function() {
     return {
       class: this.get('class'),
